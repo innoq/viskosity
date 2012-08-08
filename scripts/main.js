a65c0c35b5b384fb66edabd34e66bfde4608d96f
FND <fnd@localhost.localdomain>
2012-08-08 16:30:23 +0200

    added option to determine entry point via URI's frag ID

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-VISKOSITY.skosProvider({ uri: "data/model_building.json" }, null, function(data) {
+VISKOSITY.skosProvider({ uri: document.location.hash.substr(1) }, null, function(data) {
 	var graph = Object.create(VISKOSITY.graph);
 	graph.init("#viz", data, { height: 500, provider: VISKOSITY.skosProvider });
 });
