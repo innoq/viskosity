7033956a2ed36de25a77f20122fccc0cb226bce5
FND <fnd@localhost.localdomain>
2012-08-09 08:44:43 +0200

    tweaked scene size
    increased vertical and reduced horizontal expansion (vertical scrollbars
    resulted in horizontal scrolling becoming necessary)

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 VISKOSITY.skosProvider({ uri: document.location.hash.substr(1) }, null, function(data) {
 	var graph = Object.create(VISKOSITY.graph);
-	graph.init("#viz", data, { height: 500, provider: VISKOSITY.skosProvider });
+	graph.init("#viz", data, {
+		width: jQuery(window).width() * 0.9,
+		height: jQuery(window).height() * 0.9,
+		provider: VISKOSITY.skosProvider
+	});
 });
