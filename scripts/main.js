bef42dffc4aed90d7ba6a1dc3c779c6e901788f7
FND <fnd@localhost.localdomain>
2012-08-24 09:11:22 +0200

    created sample provider

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,9 @@
-VISKOSITY.skosProvider({ uri: document.location.hash.substr(1) }, null, function(data) {
+VISKOSITY.sampleProvider({ uri: document.location.hash.substr(1) }, null, function(data) {
 	var graph = Object.create(VISKOSITY.igraph);
 	var win = jQuery(window);
 	graph.init("#viz", data, {
 		width: win.width() * 0.9,
 		height: win.height() * 0.9,
-		provider: VISKOSITY.skosProvider
+		provider: VISKOSITY.sampleProvider
 	});
 });
